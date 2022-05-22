eeb6e4bc463d662307860a069a359166a0cf6cf0
Win Feigle <win.feigle@gmail.com>
2022-05-22 13:58:17 -0600

    commit tree 98b15573aa20bf96335b4ddcdd74946d5b32ad28

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => { 
+    return title.split(' ').map(word => {
+      return word.replace(word[0], word[0].toUpperCase())
+    }).join(' ')
+  });
 }
